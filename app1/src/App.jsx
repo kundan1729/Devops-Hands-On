11ba7bc672abee7e8b1462e7295a147f76b78604
kundan kumar <kundan234432@gmail.com>
2025-06-26 12:56:34 +0530

    multicontainer fronted microservices manged by docker compose

---
+++ app1/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <h1>this is application 1 </h1>
+        </a>
+        
+      </div>
+      <h1>Welcome here folks </h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          We'll build a microservices-style setup with three different React frontend apps running independently, all orchestrated via Docker Compose. 
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
